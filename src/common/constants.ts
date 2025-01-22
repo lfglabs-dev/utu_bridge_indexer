07c1ed5bd998103442d15abdc6726572485fc1f4
Thomas Marchand <thomas.marchand@tuta.io>
2025-01-22 16:44:36 -0100

    fix: loading starting block env vars

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -16,7 +16,13 @@ export const MONGO_CONNECTION_STRING = Deno.env.get(
   "MONGO_CONNECTION_STRING"
 ) as string;
 export const RUNES_CONTRACT = BigInt(Deno.env.get("RUNES_CONTRACT") as string);
-export const STARTING_BLOCK = BigInt(Deno.env.get("STARTING_BLOCK") as string);
+export const DEPOSITS_STARTING_BLOCK = BigInt(
+  Deno.env.get("DEPOSITS_STARTING_BLOCK") as string
+);
+export const WITHDRAWALS_STARTING_BLOCK = BigInt(
+  Deno.env.get("WITHDRAWALS_STARTING_BLOCK") as string
+);
+
 export const DATABASE_NAME = Deno.env.get("DATABASE_NAME") as string;
 
 export const DECIMALS = 18;
